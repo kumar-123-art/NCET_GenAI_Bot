233821bbcbc88c5bfef1b2bbd85ca4ca5e617191
kumar-123-art <kumar123ngi@gmail.com>
2026-04-15 10:46:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ from greg import Greg
 
 st.set_page_config("PrapgenAI Content Generator", layout="wide")
 st.title("PrapgenAI - Content Generator")
+st.image("exciting-prabhas-signs-multi-film-deal-with-salaar-makers.html")
